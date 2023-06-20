dcdcc812d2f11c5f12bd46dc7075dd0c19be5683
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-06-19 18:07:11 -0700

    failed attempt: chat with create react app

---
+++ data/code/todo-list-react-ts-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './ToDoList';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
